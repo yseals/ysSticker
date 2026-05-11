42535b2dd562e2ca19532ba9f24068c5ea04f5de
yseals <280642799+yseals@users.noreply.github.com>
2026-05-11 23:55:49 +0900

    #4 設定ファイル
    設定ファイルの読み込み、出力（exeと同じフォルダにSetting.json）
    設定ファイル保存クラス作成
    設定ファイルからフォント、色、出力文字の取得
    ラベル表示へ設定内容を適用
    usingの整理

--- src/ysSticker/FormSticker.Designer.cs
+++ src/ysSticker/FormSticker.Designer.cs
@@ -15,18 +15,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(538, 193);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(195, 155);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FormSticker
             // 
@@ -36,7 +25,6 @@
             this.CausesValidation = false;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
-            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -54,8 +42,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
