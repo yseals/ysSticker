4f7f4275ef7289a76c74daa76d75b255be1a4fe0
yseals <280642799+yseals@users.noreply.github.com>
2026-05-11 18:17:44 +0900

    #1 ステッカー表示の基盤実装 (#6)
    ・DPIスケーリング対応
    ・CreateParamsによるレイヤード/透過ウィンドウの設定
    ・フォントおよびブラシの更新処理の実装
    ・Disposeによるフォント、ブラシのリソースの解放
    ・DPIスケーリングを考慮したフォントサイズ計算
    ・ステッカー描画の基礎製作
    
    ※ボタン処理は、デバッグ確認用に残留させている
    ※フォント、ブラシの初期化処理は、設定ファイル読込み時に見直し予定

---
+++ src/ysSticker/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般的な情報は、次の方法で制御されます
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("ysSticker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ysSticker")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("0ad120ba-e845-49d7-b10a-0084f83d22bd")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+// AssemblyFileVersionではなく、製品バージョン（AssemblyInformationalVersion）を設定する
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
